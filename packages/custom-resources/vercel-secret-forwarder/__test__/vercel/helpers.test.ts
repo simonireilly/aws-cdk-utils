77fd6b1ff03ae3f0cbe7bd841753e51b172856b4
simon <simonireilly@gmail.com>
2022-01-17 07:45:03 +0000

    Typings in test suite

--- packages/custom-resources/vercel-secret-forwarder/__test__/vercel/helpers.test.ts
+++ packages/custom-resources/vercel-secret-forwarder/__test__/vercel/helpers.test.ts
@@ -1,12 +1,26 @@
 import { deleteSecretBranch } from '../../lib/vercel/helpers';
+import { VercelApi } from '../../lib/vercel/api';
 import * as nock from 'nock';
 
 const vercelGetNock = () => {
+  const envsResponse: VercelApi.GetResponse = {
+    envs: [
+      {
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        gitBranch: 'feat/integration-tests',
+        id: '11-22',
+        key: 'EXISTING_SECRET',
+        target: ['preview'],
+        type: 'plain',
+        value: 'existing-value',
+      },
+    ],
+  };
+
   nock('https://api.vercel.com:443')
     .get('/v8/projects/ab6ef546ebc4fa/env')
-    .reply(200, {
-      envs: [{}],
-    });
+    .reply(200, envsResponse);
 };
 
 const vercelPatchNock = () => {
